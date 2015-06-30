3408e185f5c9a303fbdab3536c503084fcc49462
mattphoto <github@mattkim.com>
2015-06-29 20:41:49 -0700

    add beers controller and view

--- routes/views/beers.js
+++ routes/views/beers.js
@@ -1,14 +1,42 @@
 var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
-	
-	var view = new keystone.View(req, res),
-		locals = res.locals;
-	
-	// Set locals
-	locals.section = 'beers';
-	
-	// Render the view
-	view.render('beers');
-	
+    
+    var view = new keystone.View(req,res),
+        locals = res.locals;
+
+    locals.section = 'beers';
+    locals.filters = {
+        featuredBeers: []
+    };
+
+    locals.data = {
+        allBeers: []
+    };
+
+
+    view.on('init', function(next) {
+        var q = keystone.list('Beers').model.find().where('isFeaturedBeer', true)
+
+        q.exec(function(err, result) {
+            locals.filters.featuredBeers = result;
+            next(err);
+        });
+
+    });
+
+    view.on('init', function(next) {
+
+        var q = keystone.list('Beers').model.find().sort('-beerName')
+
+        q.exec(function(err,results) {
+
+            locals.data.allBeers = results;
+            next(err);
+
+        });
+    });
+    // Render the view
+    view.render('beers');
+    
 };
