c80627702c019051758083f95b5ab8231fc79b16
Caitlin <ccmohnike@gmail.com>
2015-06-30 19:32:55 -0700

    Decided to remove all the comments from the .js file and deleted the .html file because I realized I don't actually need it.

--- routes/views/releases.js
+++ routes/views/releases.js
@@ -2,42 +2,6 @@ var keystone = require('keystone');
 
 exports = module.exports = function(req,res) {
 
-	// var view = new keystone.View(req,res),
-	// 	locals = res.locals;
-
-	// locals.section = 'brewers';
-	// locals.filters = {
-	// 	featuredBrewers: []
-	// };
-
-	// locals.data = {
-	// 	allBrewers: []
-	// };
-
-
-	// view.on('init', function(next) {
-	// 	var q = keystone.list('Homebrewers').model.find().where('isFeaturedBrewer', true)
-
-	// 	q.exec(function(err, result) {
-	// 		locals.filters.featuredBrewers = result;
-	// 		next(err);
-	// 	});
-
-	// });
-
-	// view.on('init', function(next) {
-
-	// 	var q = keystone.list('Homebrewers').model.find().sort('-brewerName')
-
-	// 	q.exec(function(err,results) {
-
-	// 		locals.data.allBrewers = results
-	// 		next(err);
-
-	// 	});
-	// });
-
-	// view.render('brewers');
 
 };
 
