f297596c9cf60e276fad7b0c30904cf92a6b81d4
mattphoto <github@mattkim.com>
2015-07-06 12:35:38 -0700

    added Beers back

---
+++ models/Beers.js
@@ -0,0 +1,34 @@
+var keystone = require('keystone'),
+Types = keystone.Field.Types;
+/**
+* Beers Model
+* ==========
+*/
+var Beers = new keystone.List('Beers', {
+  map: { name: 'beerName' },
+  autokey: { path: 'slug', from: 'beerName', unique: true },
+  defaultSort: '-brewerName'
+});
+Beers.add(
+  {heading: 'Beer'},
+  {
+    beerName: { type: String, required: true, initial: true},
+    beerType: { type: String },
+    brewer: { type: Types.Relationship, ref: 'Homebrewer', index: true },
+    beerImage: {type: Types.CloudinaryImage, required: false, initial: false },
+    isFeaturedBeer: { type: Types.Boolean, required: false},
+    initialQuantity: { type: Types.Number, required: false},
+    currentQuantity: { type: Types.Number, required: false},
+    releaseBatches: { type: Types.Relationship, ref: 'Release', index: true }
+  },
+  {heading: 'Beer Description'},
+  {
+    beerDescription: { type: Types.Textarea },
+    flavorProfile: { type: Types.Select, options: 'Malty, Earthy, Aromatic, Sweet, Rich, Delicious', default: 'Delicious', index: true, many: true }
+  }
+  );
+// Beers.schema.virtual('content.full').get(function() {
+// return this.content.extended || this.content.brief;
+// });
+Beers.defaultColumns = 'beerName, brewerName, profileDate';
+Beers.register();
