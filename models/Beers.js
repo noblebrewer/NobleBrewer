697dbfbbedb273384992ee2e88ce453d01fba045
Caitlin <ccmohnike@gmail.com>
2015-08-10 17:04:46 -0700

    Beer profile is going to be managed in Shopify. Removed extraneous fields. Will still be using keystone for managing the pictures for now.

--- models/Beers.js
+++ models/Beers.js
@@ -5,30 +5,14 @@ Types = keystone.Field.Types;
 * ==========
 */
 var Beers = new keystone.List('Beers', {
-  map: { name: 'beerName' },
-  autokey: { path: 'slug', from: 'beerName', unique: true },
-  defaultSort: '-brewerName'
+  map: { name: 'beer_name' },
+  autokey: { path: 'slug', from: 'beer_name', unique: true },
+  defaultSort: '-beer_name'
 });
-Beers.add(
-  {heading: 'Beer'},
-  {
-    beerName: { type: String, required: true, initial: true},
-    beerType: { type: String },
-    brewer: { type: Types.Relationship, ref: 'Homebrewer', index: true },
-    beerImage: {type: Types.CloudinaryImage, required: false, initial: false },
-    isFeaturedBeer: { type: Types.Boolean, required: false},
-    initialQuantity: { type: Types.Number, required: false},
-    currentQuantity: { type: Types.Number, required: false},
-    releaseBatches: { type: Types.Relationship, ref: 'Release', index: true }
-  },
-  {heading: 'Beer Description'},
-  {
-    beerDescription: { type: Types.Textarea },
-    flavorProfile: { type: Types.Select, options: 'Malty, Earthy, Aromatic, Sweet, Rich, Delicious', default: 'Delicious', index: true, many: true }
-  }
-  );
+Beers.add({
+    beer_name: { type: String },
+    cover_photo: {type: Types.CloudinaryImage}
+});
 
-Beers.relationship({ ref: 'Releases', path: 'Releases', refPath: 'beersOfRelease'});
-
-Beers.defaultColumns = 'beerName, brewerName, profileDate';
+Beers.defaultColumns = 'beer_name, cover_photo';
 Beers.register();
