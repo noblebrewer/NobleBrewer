1cc5350f710b2a2b2b176a082c4f030a62856efb
Caitlin <ccmohnike@gmail.com>
2015-08-20 11:42:23 -0700

    Hubspot data is being sent when someone registers, signs up for newsletter, or signs up to be homebrewer

--- public/javascripts/sendToHubspot.js
+++ public/javascripts/sendToHubspot.js
@@ -1,17 +1,61 @@
-$("#submit").click(function(e){
+$("#homebrewer-submit").click(function(e){
 	e.preventDefault();
 	var form = {
-		name:document.getElementById("first_name").value,
-		name:document.getElementById("last_name").value,
-		email e:document.getElementById("email").value,
-		location :document.getElementById("location").value,
-		reason :document.getElementById("reason").value,
-		recipe :document.getElementById("recipe").value,
-		awards: document.getElementById("awards").value
+		firstname : document.getElementById("first_name").value,
+		lastname : document.getElementById("last_name").value,
+		email : document.getElementById("email").value,
+		location : document.getElementById("location").value,
+		reason : document.getElementById("reason").value,
+		recipe : document.getElementById("recipe").value,
+		awards: document.getElementById("awards").value,
+		function: 'homebrewer'
+	}
+	console.log(form);
+	$.post("/api/hubspot",form,
+		function(data){
+			console.log(data);
+			if (data.status === 'error'){
+				alert('oops, an error');
+			}
+			window.location= '/success';
+		}
+	);
+	
+});
+
+$('#signup-newsletter').click(function(e){
+	e.preventDefault();
+	var form = {
+		email : document.getElementById("email").value,
+		function: 'email'
 	}
 	$.post("/api/hubspot",form,
 		function(data){
 			console.log(data);
+			if (data.status === 'error'){
+				alert('oops, an error');
+			}
+		}
+	);
+});
+
+$("#submit-registration").click(function(e){
+	alert('OHAI');
+	e.preventDefault();
+	var form = {
+		firstname : document.getElementById("first_name").value,
+		lastname : document.getElementById("last_name").value,
+		email : document.getElementById("email").value,
+		birthdate : document.getElementById("birthdate").value,
+		function: 'registration'
+	}
+	console.log(form);
+	$.post("http://localhost:3000/api/hubspot",form,
+		function(data){
+			console.log(data);
+			if (data.status === 'error'){
+				alert('oops, an error');
+			}
 			window.location= '/success';
 		}
 	);
--- routes/api/hubspot.js
+++ routes/api/hubspot.js
@@ -6,26 +6,49 @@ var request = require("request");
  * List Posts
  */
 exports = module.exports = function(req, res) {
-	var fullName = (req.body.first_name+" "+req.body.last_name);
-	var options = { method: 'POST',
-		url: 'http://api.hubapi.com/contacts/v1/contact/createOrUpdate/email/'+req.body.email+'/',
-		qs: { hapikey: keystone.get('hubspot_api') },
-		headers: { 'content-type': 'application/json' },
-		body: 
-		{ properties: 
-		  [ { property: 'email', value: req.body.email },
-		    { property: 'firstname', value: req.body.first_name },
-		    { property: 'lastname', value: req.body.last_name },
-		    { property: 'full_name', value: fullName } ] },
-		json: true };
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	if (req.body.email) {
+		var fullName = (req.body.firstname+" "+req.body.lastname);
+		var applied = 'Yes';
+		if (req.body.function === 'homebrewer') {
+			var data = { properties: 
+			  [ { property: 'email', value: req.body.email },
+			    { property: 'firstname', value: req.body.firstname },
+			    { property: 'lastname', value: req.body.lastname },
+			    { property: 'applied_to_be_a_homebrewer', value: applied},
+			    { property: 'full_name', value: fullName } ] }
+		} else if (req.body.function === 'email') {
+			var data = { properties: 
+				  [ { property: 'email', value: req.body.email } ] }
+		} else if (req.body.function === 'registration') {
+			var data = { properties: 
+				  [ { property: 'email', value: req.body.email },
+				    { property: 'firstname', value: req.body.firstname },
+				    { property: 'lastname', value: req.body.lastname },
+				    { property: 'full_name', value: fullName } ] }
+		}
 
-	request(options, function (error, response, body) {
-		if (error) throw new Error(error);
-		res.apiResponse({
-			body:req.body,
-			hubspot:body
+		var options = { method: 'POST',
+			url: 'http://api.hubapi.com/contacts/v1/contact/createOrUpdate/email/'+req.body.email+'/',
+			qs: { hapikey: keystone.get('hubspot_api') },
+			headers: { 'content-type': 'application/json' },
+			body: data,
+			json: true };
+
+		request(options, function (error, response, body) {
+			if (error) throw new Error(error);
+			console.log(body);
+			res.apiResponse({
+				body:req.body,
+				hubspot:body,
+				status:body.status
+			});
 		});
-	});
+	} else {
+		console.log('not valid email');
+	}
 };
 
 
