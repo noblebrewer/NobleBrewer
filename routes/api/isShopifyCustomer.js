5eb2e1256c1ca45ca68b26d46f3776cba2787303
Caitlin <ccmohnike@gmail.com>
2015-10-01 12:58:43 -0500

    removed the key from isShopifyCustomer. Changed the phone to be an empty string if it's undefined. Think that's what was causing the failed hubspot updates

--- routes/api/isShopifyCustomer.js
+++ routes/api/isShopifyCustomer.js
@@ -6,7 +6,7 @@ var request = require("request");
 exports = module.exports = function(req, res) {
 
 	var options = { method: 'GET',
-		url: 'https://4439f01a1abd6ec2e67c77a0444df99b:cd24b952d3551735eca0bdc27fa66ff5@noblebrewer.myshopify.com/admin/customers/search.json',
+		url: keystone.get('shopify_api')+'/admin/customers/search.json',
 		qs: { query: 'email:'+req.body.emailaddress },
 		headers: { 'content-type': 'application/json' } };
 
