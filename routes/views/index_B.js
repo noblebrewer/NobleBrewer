1b8636a92dbc69202ba290ea661923d4ab777570
Caitlin <ccmohnike@gmail.com>
2015-11-19 19:15:32 -0800

    listening for blog hostname

--- routes/views/index_B.js
+++ routes/views/index_B.js
@@ -4,6 +4,12 @@ exports = module.exports = function(req, res) {
 	
 	var view = new keystone.View(req, res),
 		locals = res.locals;
+
+	console.log(req.headers);
+	var header = req.headers;
+	if (header == 'blog') {
+		res.redirect('/blog');
+	}
 	
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
