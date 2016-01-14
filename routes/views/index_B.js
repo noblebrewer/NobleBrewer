a34048d8a08cdcf3030e48687bcfb1f7a8f1d044
Caitlin <ccmohnike@gmail.com>
2016-01-13 21:40:06 -0800

    set up experiment on the homepage - moved member benefits to the top

--- routes/views/index_B.js
+++ routes/views/index_B.js
@@ -5,6 +5,22 @@ exports = module.exports = function(req, res) {
 	var view = new keystone.View(req, res),
 		locals = res.locals;
 
+	// locals.section = 'brewers';
+	locals.filters = {
+		featuredBrewers: []
+	};
+
+
+	view.on('init', function(next) {
+		var q = keystone.list('Homebrewers').model.find().where('isFeaturedBrewer', true)
+
+		q.exec(function(err, result) {
+			locals.filters.featuredBrewers = result;
+			next(err);
+		});
+
+	});
+
 	console.log(req.headers.host);
 	var header = req.headers;
 	if (header.host == 'blog.noblebrewer.com') {
