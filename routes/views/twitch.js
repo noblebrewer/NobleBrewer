be43a48c43d57794f905bec87f30fdda864d3e72
Caitlin <ccmohnike@gmail.com>
2016-03-03 22:11:10 -0800

    added twitch redirect to the subscription flow

--- routes/views/twitch.js
+++ routes/views/twitch.js
@@ -2,39 +2,15 @@ var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
   // Render the view
-  // var shopify = keystone.get('shopify_hostname');
-  // res.redirect(shopify+'/pages/subscription');
-
-	var view = new keystone.View(req, res),
-		locals = res.locals;
-
-
-	// locals.section = 'brewers';
-	locals.filters = {
-		featuredBrewers: []
-	};
-
-
-	view.on('init', function(next) {
-		var q = keystone.list('Homebrewers').model.find().where('isFeaturedBrewer', true)
-
-		q.exec(function(err, result) {
-			locals.filters.featuredBrewers = result;
-			next(err);
-		});
-
-	});
-
-
-	console.log(req.headers.host);
-	var header = req.headers;
-	if (header.host == 'blog.noblebrewer.com') {
-		res.redirect('/blog');
-	}
 	
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'home';
-
-  	view.render('index')
+  	console.log(req);
+	var utms = {
+		source : req.query.utm_source,
+		medium : req.query.utm_medium,
+		content : req.query.utm_content,
+		campaign : req.query.utm_campaign
+	}
+	console.log(utms);
+  var shopify = keystone.get('shopify_hostname');
+  res.redirect(shopify+'/pages/twitch?utm_source='+utms.source+'&utm_medium='+utms.medium+'&utm_content='+utms.content+'&utm_campaign='+utms.campaign);
 };
