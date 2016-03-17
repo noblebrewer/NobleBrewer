443985caead88245087ece9be1d6029573c667b4
Caitlin <ccmohnike@gmail.com>
2016-03-16 17:02:05 -0700

    fixed the redirect to keep the utm params

--- routes/views/landingpage_untappd.js
+++ routes/views/landingpage_untappd.js
@@ -5,7 +5,7 @@ exports = module.exports = function(req, res) {
 	var view = new keystone.View(req, res),
 		locals = res.locals;
 
-	
+	console.log(req.query);
 	// console.log(location, version, source);
 
 	locals.data = {
@@ -36,7 +36,14 @@ exports = module.exports = function(req, res) {
 	locals.section = 'Untappd Noble Brewer';
 	
 	var shopify = keystone.get('shopify_hostname');
-	res.redirect(shopify+'/pages/noble-brewer-samplers?untappd_location='+req.params.location+'&untappd_version='+req.params.version+'&untappd_source='+req.params.source);
+	res.redirect(shopify+'/pages/noble-brewer-samplers?untappd_location='+req.params.location
+		+'&untappd_version='+req.params.version
+		+'&untappd_source='+req.params.source
+		+'&utm_content='+req.query.utm_content
+		+'&utm_term='+req.query.utm_term
+		+'&utm_campaign='+req.query.utm_campaign
+		+'&utm_medium='+req.query.utm_medium
+		+'&utm_source='+req.query.utm_source);
 
 	// Render the view
 	// view.render('landingpage_untappd');
