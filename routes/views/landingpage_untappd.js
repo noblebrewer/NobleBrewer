2988c0f2421853a3962338004a69f50e113bb4cb
Caitlin <ccmohnike@gmail.com>
2016-04-08 15:28:31 -0700

    changed untappd to use the same landing page style as the facebook traffic

--- routes/views/landingpage_untappd.js
+++ routes/views/landingpage_untappd.js
@@ -5,47 +5,45 @@ exports = module.exports = function(req, res) {
 	var view = new keystone.View(req, res),
 		locals = res.locals;
 
-	console.log(req.query);
-	// console.log(location, version, source);
+	// locals.section = 'brewers';
+	locals.filters = {
+		featuredBrewers: []
+	};
 
-	locals.data = {
-		location : req.params.location,
-		version : req.params.version,
-		source : req.params.source
+
+	view.on('init', function(next) {
+		var q = keystone.list('Homebrewers').model.find().where('isFeaturedBrewer', true)
+
+		q.exec(function(err, result) {
+			locals.filters.featuredBrewers = result;
+			next(err);
+		});
+
+	});
+
+	console.log(req.headers.host);
+	var header = req.headers;
+	if (header.host == 'blog.noblebrewer.com') {
+		res.redirect('/blog');
 	}
-
-	// heap.track('untappd', 
-	// 	{ 	city: req.params.location, 
-	// 		version: req.params.version,
-	// 		source : req.params.source });
-
-	// console.log(locals.data);
-
-	// var location = req.params.location;
-	// var version = req.params.version;
-	// var source = req.params.source;
-
-
-	//TODO: Send event that the page was viewed?
-	//TODO: Send event when email sign up (with location)
-	//TODO: Not sure if we need those
-
 	
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'Untappd Noble Brewer';
 	
-	var shopify = keystone.get('shopify_hostname');
-	res.redirect(shopify+'/pages/noble-brewer-samplers?untappd_location='+req.params.location
-		+'&untappd_version='+req.params.version
-		+'&untappd_source='+req.params.source
-		+'&utm_content='+req.query.utm_content
-		+'&utm_term='+req.query.utm_term
-		+'&utm_campaign='+req.query.utm_campaign
-		+'&utm_medium='+req.query.utm_medium
-		+'&utm_source='+req.query.utm_source);
-
 	// Render the view
-	// view.render('landingpage_untappd');
+	view.render('landingpage_untappd');
 	
 };
+	
+	// locals.section = 'Untappd Noble Brewer';
+	
+	// var shopify = keystone.get('shopify_hostname');
+	// res.redirect(shopify+'/pages/noble-brewer-samplers?untappd_location='+req.params.location
+	// 	+'&untappd_version='+req.params.version
+	// 	+'&untappd_source='+req.params.source
+	// 	+'&utm_content='+req.query.utm_content
+	// 	+'&utm_term='+req.query.utm_term
+	// 	+'&utm_campaign='+req.query.utm_campaign
+	// 	+'&utm_medium='+req.query.utm_medium
+	// 	+'&utm_source='+req.query.utm_source);
