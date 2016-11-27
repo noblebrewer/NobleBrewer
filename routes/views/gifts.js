a4b0865c106b7887e4cc062355ca5efc7a278247
Caitlin <ccmohnike@gmail.com>
2016-11-27 14:50:26 -0800

    gifts flow

--- routes/views/gifts.js
+++ routes/views/gifts.js
@@ -1,7 +1,21 @@
 var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
-  // Render the view
-  var shopify = keystone.get('shopify_hostname');
-  res.redirect(shopify+'/pages/all-products');
+
+	var view = new keystone.View(req, res),
+		locals = res.locals;
+
+	locals.data = {
+		source: req.query.utm_source,
+		medium: req.query.utm_medium,
+		campaign: req.query.utm_campaign,
+		content: req.query.utm_content,
+		term: req.query.utm_term
+	}
+	
+	// Set locals
+	locals.section = 'gifts';	
+	
+	// Render the view
+	view.render('gifts');
 };
