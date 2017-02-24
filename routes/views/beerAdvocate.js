a4d291f5464714791e39f184ffe0ca9a1630a556
Caitlin <ccmohnike@gmail.com>
2017-02-23 18:42:49 -0800

    removed the featured brewers loop

--- routes/views/beerAdvocate.js
+++ routes/views/beerAdvocate.js
@@ -5,17 +5,6 @@ exports = module.exports = function(req, res) {
 	var view = new keystone.View(req, res),
 		locals = res.locals;
 	
-	view.on('init', function(next) {
-		var q = keystone.list('Homebrewers').model.find().where('isFeaturedBrewer', true)
-
-		q.exec(function(err, result) {
-			locals.filters.featuredBrewers = result;
-			next(err);
-		});
-
-	});
-
-	
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
