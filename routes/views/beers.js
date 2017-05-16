93513c4f81e3e77292302414404c4624c051764a
Caitlin <ccmohnike@gmail.com>
2017-05-15 19:44:11 -0700

    changed header link for the beer page

--- routes/views/beers.js
+++ routes/views/beers.js
@@ -4,7 +4,7 @@ var keystone = require('keystone'),
 exports = module.exports = function(req, res) {
   // Render the view
   var shopify = keystone.get('shopify_hostname');
-  res.redirect(shopify+'/collections/beer');
+  res.redirect(shopify+'/pages/noble-brewer-beers');
 };
 
 
